90fda8764e0f3437521e805118ff3217468d862c
Ronen Gershon Cohen <sronenco@gmail.com>
2021-04-01 21:16:29 +0300

    Updated code to print in the server's console a list of the existing DBs.

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const connection = "mongodb+srv://m001-student:m001-mongodb-basics@sandbox.lqxqp.mongodb.net/admin?retryWrites=true&w=majority";
+const connection = process.env.MONGO_DB_URI || "mongodb+srv://m001-student:m001-mongodb-basics@sandbox.lqxqp.mongodb.net/sample_airbnb?retryWrites=true&w=majority";
 mongoose.connect(connection, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
 	.then(() => console.log("Database Connected Successfully"))
 	.catch(err => console.log(err));
--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 	.get('/', (req, res) => res.render('pages/index'));
 
 // API Definition
-const users = require('/api/users');
+const users = require('./api/users');
 app.use('/api/users', users);
 
 // API Implementation
@@ -26,16 +26,43 @@ app.get('*', (req, res) => {
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-/*
+// MongoDB official documentation
 const MongoClient = require('mongodb').MongoClient;
-const uri = process.env.MONGO_DB_URI || 'mongodb+srv://m001-student:m001-mongodb-basics@sandbox.lqxqp.mongodb.net/admin?retryWrites=true&w=majority';
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("sample_training").collection("zips");
 
-  // perform actions on the collection object
-  console.log(collection.findOne());
-  
-  client.close();
-});
-*/
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+ 
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+async function main() {
+	/**
+	 * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+	 * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+	 */
+	const uri = process.env.MONGO_DB_URI || 'mongodb+srv://m001-student:m001-mongodb-basics@sandbox.lqxqp.mongodb.net/sample_airbnb?retryWrites=true&w=majority';
+	
+	const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+	try	{
+		// Connect to the MongoDB cluster
+		await client.connect();
+
+		// Make the appropriate DB calls
+    	await listDatabases(client);
+		
+		// await client.connect(err => {
+		// 	const collection = client.db("sample_training").collection("zips");
+		// 	// perform actions on the collection object
+		// 	console.log(collection.findOne());
+		// 	client.close();
+		// });
+	} catch (e) {
+		console.error(e);
+	} finally {
+		await client.close();
+	}
+}
+
+main().catch(console.error);
