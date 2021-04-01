96c7e61e6ab98f97c5ca16c37cdc6d1b7e7ce224
Ronen Gershon Cohen <sronenco@gmail.com>
2021-04-01 12:13:26 +0300

    Added API (definition and implementation), Port and other code from the tutorial at https://www.freecodecamp.org/news/deploying-a-mern-application-using-mongodb-atlas-to-heroku/#creating-the-backend

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const bodyParser = require('body-parser');	//FCC
+const cors = require('cors');				//FCC
+const path = require('path');
+const app = express();						//FCC
+require('./database');						//FCC
+
+app.use(express.static(path.join(__dirname, 'public')))
+	.use(bodyParser.json())
+	.use(cors())
+	.set('views', path.join(__dirname, 'views'))
+	.set('view engine', 'ejs')
+	.get('/', (req, res) => res.render('pages/index'));
+
+// API Definition
+const users = require('/api/users');
+app.use('/api/users', users);
+
+// API Implementation
+app.use(express.static(path.join(__dirname, '../build')))
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '../build'))
+})
+
+// Port and Listen
 const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-
-console.log('Defining MongoDB');
-
+/*
 const MongoClient = require('mongodb').MongoClient;
 const uri = process.env.MONGO_DB_URI || 'mongodb+srv://m001-student:m001-mongodb-basics@sandbox.lqxqp.mongodb.net/admin?retryWrites=true&w=majority';
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -23,3 +38,4 @@ client.connect(err => {
   
   client.close();
 });
+*/
